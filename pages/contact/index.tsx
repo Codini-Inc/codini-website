e48406ccf330068a5424c92bf3542983db787350
JihedKhelifi <62301211+Jihed-khelifi@users.noreply.github.com>
2024-02-28 01:21:50 +0100

    refactor(project): added tailwind

---
+++ pages/contact/index.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+
+export const Contact: React.FC = () => {
+  return (
+    <>
+      <Head>
+        <title>Codini | Contact</title>
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content="Codini Website" />
+        <meta property="og:title" content="Codini | Home" />
+      </Head>
+      <main className=''>
+        <h1>Contact</h1>
+        <div></div>
+        <div></div>
+        <div></div>
+      </main>
+    </>
+  );
+};
+
+export default Contact;
