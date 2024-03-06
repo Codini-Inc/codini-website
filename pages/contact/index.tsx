4936bd7847fc23dca86ff0de3b9c0343d839e66f
Chadi Ayari <40720094+chadiayari@users.noreply.github.com>
2024-03-06 14:52:31 +0100

    added contact page

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,18 +1,62 @@
 import Head from 'next/head';
+import styles from '../../components/blog/NewsletterCta.module.css';
+import cn from 'classnames';
+import { ContactForm } from '../../components/contact/contactForm';
+import { Seeking } from '../../components/contact/Seeking';
+import { ClientTypeSelection } from '../../components/contact/ClientTypeSelection';
+import gsap from 'gsap';
+import { useEffect, useState, useRef } from 'react';
+import { RefObject } from 'react';
+import { ClientNeeds, ClientType } from '../../_enums';
 
 export const Contact: React.FC = () => {
-  async function handlesubmit(contact: any) {
-    contact.prevenDefault();
+  const clientTypeSelectionRef = useRef<HTMLDivElement>(null);
+  const seekingRef = useRef<HTMLDivElement>(null);
+  const contactFormRef = useRef<HTMLDivElement>(null);
+  const [clientType, setClientType] = useState<ClientType | null>(null);
+  const [clientNeed, setClientNeed] = useState<ClientNeeds | null>(null);
+  const [animationStartStepOne, setAnimationStartStepOne] = useState(false);
 
-    const data = {
-      type: String(contact.target.type.value),
-      puropose: String(contact.target.puropose.value),
-      phonenumber: String(contact.target.phonenumber.value),
-      email: String(contact.target.email.value),
-    };
+  const animateStep = (
+    ref: RefObject<HTMLDivElement>,
+    delay: number,
+    isEnter: boolean,
+  ) => {
+    gsap.fromTo(
+      ref.current,
+      {
+        x: isEnter ? 3000 : 0,
 
-    console.log(data);
-  }
+        opacity: isEnter ? 0 : 1,
+      },
+      {
+        x: isEnter ? 0 : -3000,
+        opacity: isEnter ? 1 : 0,
+        duration: 1.3,
+        delay: delay,
+        ease: 'power4.out',
+      },
+    );
+  };
+
+  useEffect(() => {
+    animateStep(clientTypeSelectionRef, 0, true);
+  }, []);
+
+  useEffect(() => {
+    if (clientType === null) return;
+    animateStep(clientTypeSelectionRef, 0, false);
+  }, [clientType]);
+
+  useEffect(() => {
+    if (clientType !== null) {
+      animateStep(seekingRef, 0.1, true);
+    }
+    if (clientNeed !== null) {
+      animateStep(seekingRef, 0, false);
+    }
+  }, [clientType, clientNeed]);
+
   return (
     <>
       <Head>
@@ -21,36 +65,29 @@ export const Contact: React.FC = () => {
         <meta property="og:description" content="Codini Website" />
         <meta property="og:title" content="Codini | Home" />
       </Head>
-      <div className="p-4 max-w-3xl mx-auto">
-        <h1>Contact Us</h1>
-        <form>
-          <div className="w-full flex flex-col my-4">
-            <label className="font-hold text-gray-800" htmlFor="type" >Type</label>
-            <input type="text" autoComplete='off' id="type" />
-
+      <main className="w-full flex mt-20 h-screen space-y-20 items-center justify-center ">
+        {clientType === null && (
+          <div className="w-full h-full " ref={clientTypeSelectionRef}>
+            <ClientTypeSelection
+              setClientType={setClientType}
+              clientType={clientType as ClientType}
+            />
           </div>
-          <div className="w-full flex flex-col my-4">
-            <label className="font-hold text-gray-800" htmlFor="puropose" >Puropose</label>
-            <input type="text" autoComplete="off" id="purpose" />
-
+        )}
+        {clientType !== null && clientNeed === null && (
+          <div className="w-full h-full" ref={seekingRef}>
+            <Seeking
+              setClientNeed={setClientNeed}
+              clientNeed={clientNeed as ClientNeeds}
+            />
           </div>
-          <div className="w-full flex flex-col my-4">
-            <label className="font-hold text-gray-800" htmlFor="phonenumber" >Phone Number</label>
-            <input type="text" autoComplete="off" id="phonenumber" />
-
+        )}
+        {clientNeed !== null && (
+          <div className="w-full h-full" ref={contactFormRef}>
+            <ContactForm />
           </div>
-          <div className="w-full flex flex-col my-4">
-            <label className="font-hold text-gray-800" htmlFor="email" >Email</label>
-            <input type="text" autoComplete="off" id="email" />
-          </div>
-          <div>
-            <textarea rows={4} placeholder="How we can help you?"
-              className="w-full" />
-          </div>
-          <button type="submit" className="px-4 py-2 w-24 bg-gray-700 text-white font-medium rounded">Send</button>
-        </form >
-        <p>Please fill in the form below</p>
-      </div >
+        )}
+      </main>
     </>
   );
 };
