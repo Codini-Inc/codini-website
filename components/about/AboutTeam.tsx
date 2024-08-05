74ae018c7a2d216c189ca867e66b1da26d6de50c
Chadi Ayari <40720094+chadiayari@users.noreply.github.com>
2024-08-05 22:38:46 +0100

    updated about section

--- components/about/AboutTeam.tsx
+++ components/about/AboutTeam.tsx
@@ -18,7 +18,7 @@ const AboutTeam: NextPage = () => {
             <div className={styles['about__role-teammember']}>
               Founder &amp; CEO
             </div>
-            <div className={styles['about__socialwrapper-teammember']}>
+            {/* <div className={styles['about__socialwrapper-teammember']}>
               <a
                 href="#"
                 className={cn(
@@ -29,7 +29,7 @@ const AboutTeam: NextPage = () => {
                 <div
                   className={cn(styles['about__smicon-teammember'], 'w-embed')}
                 >
-                  {/* <svg
+                  <svg
                     width="26"
                     height="26"
                     viewBox="0 0 26 26"
@@ -42,7 +42,7 @@ const AboutTeam: NextPage = () => {
                       d="M8.31829 23.3331C17.8228 23.3331 23.0236 15.4513 23.0236 8.62782C23.0236 8.40609 23.0236 8.18435 23.0135 7.96261C24.0214 7.23692 24.8983 6.31973 25.5938 5.28159C24.6665 5.69483 23.6687 5.96696 22.6204 6.09799C23.6888 5.46301 24.5052 4.44503 24.8983 3.23554C23.9005 3.83021 22.7918 4.25352 21.6125 4.48534C20.6651 3.47744 19.3246 2.85254 17.843 2.85254C14.9906 2.85254 12.6724 5.17072 12.6724 8.02308C12.6724 8.42624 12.7228 8.81933 12.8035 9.20233C8.50979 8.99067 4.69992 6.92447 2.14992 3.79997C1.70644 4.56597 1.45447 5.45293 1.45447 6.40036C1.45447 8.19443 2.37166 9.77683 3.75249 10.7041C2.90585 10.6739 2.10961 10.442 1.41415 10.059C1.41415 10.0792 1.41415 10.0994 1.41415 10.1296C1.41415 12.6292 3.19814 14.7256 5.55663 15.1994C5.12324 15.3203 4.66968 15.3808 4.19597 15.3808C3.86336 15.3808 3.54083 15.3505 3.22838 15.2901C3.88352 17.3462 5.79853 18.8379 8.05623 18.8782C6.28233 20.2691 4.05486 21.0956 1.63589 21.0956C1.22265 21.0956 0.809411 21.0754 0.40625 21.025C2.68411 22.4764 5.40545 23.3331 8.31829 23.3331Z"
                       fill="currentColor"
                     ></path>
-                  </svg> */}
+                  </svg>
                   <Twitter />
                 </div>
               </a>
@@ -56,7 +56,7 @@ const AboutTeam: NextPage = () => {
                 <div
                   className={cn(styles['about__smicon-teammember'], 'w-embed')}
                 >
-                  {/* <svg
+                  <svg
                     width="26"
                     height="26"
                     viewBox="0 0 26 26"
@@ -69,15 +69,15 @@ const AboutTeam: NextPage = () => {
                       d="M21.2977 21.2966H17.8037V15.8274C17.8037 14.5237 17.7802 12.8468 15.9867 12.8468C14.1697 12.8468 13.8921 14.2684 13.8921 15.7345V21.2966H10.4008V10.052H13.7494V11.5888H13.7979C14.2652 10.7052 15.4041 9.77319 17.1047 9.77319C20.6445 9.77319 21.2977 12.1006 21.2977 15.1297V21.2966ZM6.46038 8.51658C5.33717 8.51658 4.43127 7.60812 4.43127 6.4889C4.43127 5.37099 5.33717 4.46252 6.46038 4.46252C7.57704 4.46252 8.48556 5.37099 8.48556 6.4889C8.48556 7.60812 7.57704 8.51658 6.46038 8.51658ZM4.7088 21.2967H8.20803V10.0521H4.7088V21.2967ZM23.0375 1.21875H2.95593C1.99766 1.21875 1.21875 1.9793 1.21875 2.91787V23.0808C1.21875 24.0194 1.99766 24.7812 2.95593 24.7812H23.0375C23.9984 24.7812 24.7812 24.0194 24.7812 23.0808V2.91787C24.7812 1.9793 23.9984 1.21875 23.0375 1.21875Z"
                       fill="currentColor"
                     ></path>
-                  </svg> */}
+                  </svg>
                   <Linkedin />
                 </div>
               </a>
-            </div>
+            </div> */}
           </div>
           <div className={styles['about__overlay-teammember']}></div>
           <img
-            src="/images/chadi.jpg"
+            src="/images/chadi.jpeg"
             width={900}
             height={500}
             alt="Chadi Ayari, CEO & Founder"
@@ -104,7 +104,7 @@ const AboutTeam: NextPage = () => {
             <div className={styles['about__role-teammember']}>
               Marketing Manager
             </div>
-            <div className={styles['about__socialwrapper-teammember']}>
+            {/* <div className={styles['about__socialwrapper-teammember']}>
               <a
                 href="#"
                 className={cn(
@@ -118,7 +118,7 @@ const AboutTeam: NextPage = () => {
                   <Linkedin />
                 </div>
               </a>
-            </div>
+            </div> */}
           </div>
           <div className={styles['about__overlay-teammember']}></div>
           <img
@@ -138,7 +138,7 @@ const AboutTeam: NextPage = () => {
               Full stack developer
               <br />
             </div>
-            <div className={styles['about__socialwrapper-teammember']}>
+            {/* <div className={styles['about__socialwrapper-teammember']}>
               <a
                 href="#"
                 className={cn(
@@ -152,7 +152,7 @@ const AboutTeam: NextPage = () => {
                   <Linkedin />
                 </div>
               </a>
-            </div>
+            </div> */}
           </div>
           <div className={styles['about__overlay-teammember']}></div>
           <img
@@ -173,7 +173,7 @@ const AboutTeam: NextPage = () => {
               Designer
               <br />
             </div>
-            <div className={styles['about__socialwrapper-teammember']}>
+            {/* <div className={styles['about__socialwrapper-teammember']}>
               <a
                 href="#"
                 className={cn(
@@ -187,7 +187,7 @@ const AboutTeam: NextPage = () => {
                   <Linkedin />
                 </div>
               </a>
-            </div>
+            </div> */}
           </div>
           <div className={styles['about__overlay-teammember']}></div>
           <img
