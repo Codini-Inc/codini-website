aa7ba8b72114d872e3cfb713306b5e480edec5ba
Chadi Ayari <40720094+chadiayari@users.noreply.github.com>
2025-05-07 14:59:01 +0100

    Update Testimonials.tsx

--- components/home/Testimonials.tsx
+++ components/home/Testimonials.tsx
@@ -12,7 +12,9 @@ const Testimonials: NextPage = () => {
         <div className={styles.testimonials__card}>
           <div className={styles['testimonials__card-content']}>
             <div className={styles['testimonials__card-blockquote']}>
-            Working with Codini was a game-changer for our online presence. Their expertise resulted in a website that surpassed our expectations in the medical industry.
+              Working with Codini was a game-changer for our online presence.
+              Their expertise resulted in a website that surpassed our
+              expectations in the medical industry.
             </div>
             <div className={styles['testimonials__card-info']}>
               <Image
@@ -27,8 +29,13 @@ const Testimonials: NextPage = () => {
                 Karim Bouattour
               </h4>
               <div className={styles['testimonials__card-role']}>
-                <Link href="https://www.sotcot.tn/" target="_blank" className={styles['role_links']} aria-label="Sotcot">
-                Head of SOTCOT, Tunisia
+                <Link
+                  href="https://www.sotcot.tn/"
+                  target="_blank"
+                  className={styles['role_links']}
+                  aria-label="Sotcot"
+                >
+                  Head of SOTCOT, Tunisia
                 </Link>
               </div>
             </div>
@@ -37,32 +44,9 @@ const Testimonials: NextPage = () => {
         <div className={styles.testimonials__card}>
           <div className={styles['testimonials__card-content']}>
             <div className={styles['testimonials__card-blockquote']}>
-            Codini exceeded our expectations with their expertise, and seamless communication, delivering a top-notch mobile app on App Store and Play Store.
-            </div>
-            <div className={styles['testimonials__card-info']}>
-              <Image
-                src="/images/second-testimonial.jpeg"
-                loading="lazy"
-                alt=""
-                className={styles['testimonials__card-photo']}
-                width="120"
-                height="120"
-              />
-              <h4 className={styles['testimonials__card-heading']}>
-                Roy Arwas
-              </h4>
-              <div className={styles['testimonials__card-role']}>
-                <Link href="https://www.creativexent.com/home" target="_blank" className={styles['role_links']} aria-label="Creative X entertainement">
-                  CEO at CXE, Miami
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className={styles.testimonials__card}>
-          <div className={styles['testimonials__card-content']}>
-            <div className={styles['testimonials__card-blockquote']}>
-            Thanks to Codini, our web app has been transformed into a reliable platform. Their expertise resolved our issues swiftly, ensuring smooth functionality and boosting performance.
+              Thanks to Codini, our web app has been transformed into a reliable
+              platform. Their expertise resolved our issues swiftly, ensuring
+              smooth functionality and boosting performance.
             </div>
             <div className={styles['testimonials__card-info']}>
               <Image
@@ -77,7 +61,12 @@ const Testimonials: NextPage = () => {
                 Benedict Rees
               </h4>
               <div className={styles['testimonials__card-role']}>
-                <Link className={styles['role_links']} href="https://compareprivateplanes.com" target="_blank" aria-label="Comoare Private Planes">
+                <Link
+                  className={styles['role_links']}
+                  href="https://compareprivateplanes.com"
+                  target="_blank"
+                  aria-label="Comoare Private Planes"
+                >
                   CEO at CPP, London
                 </Link>
               </div>
